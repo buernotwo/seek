c24fa96b1c8c7fa4ed6bbaa1be27dbe7391ad0d4
buer <zk_cwzy@163.com>
2017-11-20 22:46:16 +0800

    修改主页页面显示，删除部分代码。

--- src/seeyou/controller/FQueryServlet.java
+++ src/seeyou/controller/FQueryServlet.java
@@ -8,9 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class FQueryServlet extends HttpServlet{
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	/**
 	 * @throws ServletException 
